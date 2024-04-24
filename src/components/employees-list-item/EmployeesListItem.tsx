332e79d3b075e255444d1a88cc7e1a0f15109127
Juray <juraypodusovskyy@gmail.com>
2024-04-24 09:27:40 +0300

    practice

--- src/components/employees-list-item/EmployeesListItem.tsx
+++ src/components/employees-list-item/EmployeesListItem.tsx
@@ -1,33 +1,35 @@
-import {FC,PropsWithChildren} from 'react';
+import React, { FC, PropsWithChildren, useState } from 'react';
 
-import'./employeesListItem.css';
+import './employeesListItem.css';
 import { IEmployee } from '../../interfaces/employees-interface';
 
 
-interface IProps extends PropsWithChildren{
-  employee:IEmployee
+interface IProps extends PropsWithChildren {
+  employee: IEmployee;
 }
 
-const EmployeesListItem:FC<IProps> = ({employee:{increase,name,salary}}) => {
+const EmployeesListItem: FC<IProps> = ({ employee: { increase, name, salary } }) => {
+  const [currentIncrease, setIncrease] = useState<boolean>(increase);
+  const [like, setLike] = useState<boolean>(false);
 
-  let className = 'list-group-item d-flex justify-content-between';
+  const listItemClasses = `list-group-item d-flex justify-content-between${currentIncrease ? ' increase' : ''}${like ? ' like' : ''}`;
 
-  if(increase){
-    className += ' increase';
-  }
+  const onIncrease = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+    e.stopPropagation();
+    setIncrease(prev => !prev);
+  };
+
+  const onLike = () => setLike(prev => !prev);
 
   return (
-    <li className={className}>
+    <li className={listItemClasses} onClick={onLike}>
       <span className="list-group-item-label">{name}</span>
-      <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+      <input type="text" className="list-group-item-input" defaultValue={`${salary}$`} />
       <div className='d-flex justify-content-center align-items-center'>
-        <button type="button"
-          className="btn-cookie btn-sm ">
+        <button type="button" className="btn-cookie btn-sm" onClick={onIncrease}>
           <i className="fas fa-cookie"></i>
         </button>
-
-        <button type="button"
-          className="btn-trash btn-sm ">
+        <button type="button" className="btn-trash btn-sm">
           <i className="fas fa-trash"></i>
         </button>
         <i className="fas fa-star"></i>
